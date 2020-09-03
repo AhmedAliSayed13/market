0c50a3bc61bb4a2be44fef8a0b89458544f4d28a
pksaudi4 <68017997+pksaudi4@users.noreply.github.com>
2020-09-03 16:14:16 +0200

    first commit

--- database/factories/ProductimageFactory.php
+++ database/factories/ProductimageFactory.php
@@ -5,7 +5,7 @@
 use App\Model;
 use Faker\Generator as Faker;
 
-$factory->define(\App\Models\Proudctimage::class, function (Faker $faker) {
+$factory->define(\App\Models\Productimage::class, function (Faker $faker) {
     return [
         'image' => 'product.png',
         'product_id' => $faker->unique()->numberBetween(1,100),
